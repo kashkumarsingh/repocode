ec572b84d43b29ac62795e4c09bb3bd10ec12b44
Kash <121066591+kashkumarsingh@users.noreply.github.com>
2024-10-17 13:17:43 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/main.css">
+</head>
+
+<body>
+
+    <form class="form" action="process-form.php" method="post" novalidate>
+        <div class="form__group">
+            <label for="name" class="form__label">Name</label>
+            <input type="text" id="name" name="name" class="form__input" required aria-required="true"
+                aria-label="Name">
+        </div>
+        <div class="form__group">
+            <label for="email" class="form__label">Email</label>
+            <input type="email" id="email" name="email" class="form__input" required aria-required="true"
+                aria-label="Email">
+        </div>
+        <div class="form__group">
+            <label for="password" class="form__label">Password</label>
+            <input type="password" id="password" name="password" class="form__input" required aria-required="true"
+                aria-label="Password">
+        </div>
+        <button type="submit" class="form__button">Register</button>
+    </form>
+
+</body>
+
+</html>
